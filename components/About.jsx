8f8c9fc954844a8c9e68660f6f8ef9dae7f364cb
Udanth <udanth.work@gmail.com>
2025-10-02 14:57:27 +0530

    init

---
+++ components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h4>Intoduction</h4>
+      <h2>About Me</h2>
+    </div>
+  )
+}
+
+export default About
---
+++ components/Navbar.jsx
@@ -0,0 +1,94 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import { useRef } from "react";
+
+const Navbar = () => {
+
+    const sideMenuRef = useRef();
+
+    const openMenu = () =>{
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+
+    const closeMenu = () =>{
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
+    return (
+        <>
+            <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+                <Image src={assets.header_bg_color} alt="" className="w-full" />
+            </div>
+            <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+                <a href="#top">
+                    <Image
+                        src={assets.logo}
+                        width={112}
+                        height={32}
+                        className="cursor-pointer mr-14"
+                        alt="Logo"
+                    />
+                </a>
+                <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+                    <li>
+                        <a className="font-Ovo" href="#top">Home</a>
+                    </li>
+                    <li>
+                        <a className="font-Ovo" href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a className="font-Ovo" href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a className="font-Ovo" href="#contact">Contact</a>
+                    </li>
+                </ul>
+
+                <div className="flex items-center gap-4">
+
+                    <button>
+                        <Image src={assets.moon_icon} alt="" className="w-6" />
+                    </button>
+
+                    <a
+                        href="#contact"
+                        className="font-Ovo hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+                    >
+                        Contact{" "}
+                        <Image
+                            src={assets.arrow_icon}
+                            width={12}
+                            height={12}
+                            alt="Arrow icon"
+                        />
+                    </a>
+
+                    <button onClick={openMenu} className="block md:hidden ml-4 cursor-pointer">
+                        <Image src={assets.menu_black} alt="" className="w-6 " />
+                    </button>
+                </div>
+
+                {/* mobile */}
+                <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
+                    <div onClick={closeMenu} className="absolute right-6 top-6">
+                        <Image  src={assets.close_black} alt="" className="w-5 cursor-pointer" />
+                    </div>
+                    <li>
+                        <a onClick={closeMenu} className="font-Ovo" href="#top">Home</a>
+                    </li>
+                    <li>
+                        <a onClick={closeMenu} className="font-Ovo" href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a onClick={closeMenu} className="font-Ovo" href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a onClick={closeMenu} className="font-Ovo" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
